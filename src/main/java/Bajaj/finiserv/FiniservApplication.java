f584ae88b684be1da1c4c92a8396a29f256b1242
divya bonde <divyabonde12@gmail.com>
2022-04-28 22:21:20 +0530

    first commit

---
+++ src/main/java/Bajaj/finiserv/FiniservApplication.java
@@ -0,0 +1,15 @@
+package Bajaj.finiserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class FiniservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiniservApplication.class, args);
+	}
+
+}
